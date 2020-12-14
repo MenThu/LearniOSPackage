2fd1f1ce93ead692e04a807a2b9f47e5da48c5d5
menthuguan <menthuguan@tencent.com>
2020-12-14 12:49:47 +0800

    mark:了解如何使用脚本来打包iOS应用程序

---
+++ AppPackage/AppPackage/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AppPackage
+//
+//  Created by menthu on 2020/12/14.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
